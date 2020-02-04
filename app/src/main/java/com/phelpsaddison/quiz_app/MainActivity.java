3de0c85c7ae6f035034808fb41d7893cf58d39e7
Addison Phelps <addyp47@gmail.com>
2020-02-04 10:08:49 -0500

    Initial commit

---
+++ app/src/main/java/com/phelpsaddison/quiz_app/MainActivity.java
@@ -0,0 +1,48 @@
+package com.phelpsaddison.quiz_app;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    Button submitButton;
+    EditText responseText;
+    TextView displayText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        submitButton=findViewById(R.id.clickButton);
+        responseText=findViewById(R.id.responseEditText);
+        displayText=findViewById(R.id.textBox);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Hello mom");
+                Log.i("testButton","Hi! "+responseText.getText().toString());
+                System.out.println(displayText.getText().toString());
+                displayText.setText(displayText.getText().toString()+" "+responseText.getText().toString());
+                displayText.setTextColor(Color.parseColor(displayText.getText().toString()));
+
+            }
+        });
+        responseText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus){
+                    if(responseText.getText().toString().equals("TJ")){
+                        displayText.setText("TJ Rocks!");
+                        responseText.setText("");
+                        responseText.setHint("That's a good name.");
+                    }
+                }
+            }
+        });
+    }
+
+}
